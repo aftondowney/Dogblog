3d91e810339c81dcae5bee1dfbc3751aea90e192
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-03-30 15:38:41 -0700

    rebuild app after scrapping

---
+++ app/models/entry.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  content: DS.attr(),
+  date: DS.attr(),
+  author: DS.attr(),
+  image: DS.attr(),
+  blurb: DS.attr()
+});
