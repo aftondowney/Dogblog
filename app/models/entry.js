02b643fc48df0a4ff60c0487c3d9c734e0e7474d
Afton Downey and Bri Popson <bpopson@gmail.com>
2016-03-31 09:20:42 -0700

    and comment model that links to entries

--- app/models/entry.js
+++ app/models/entry.js
@@ -6,5 +6,6 @@ export default DS.Model.extend({
   date: DS.attr(),
   author: DS.attr(),
   image: DS.attr(),
-  blurb: DS.attr()
+  blurb: DS.attr(),
+  comments: DS.hasMany('comment', {async: true})
 });
